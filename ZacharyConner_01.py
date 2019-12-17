71a31b6f537a0f2c7f9f53099237d921f5ebf8d9
ThatGuyCoot <50492150+zconner-09@users.noreply.github.com>
2019-12-17 12:07:00 -0500

    Add files via upload
    Added the .py file for assignment 1

---
+++ ZacharyConner_01.py
@@ -0,0 +1,32 @@
+import platform
+
+Zack_Conner = "Second year MS student in Geoinformatics. Background in Environmental Science"
+Interests = "Currently interning at the DOT in the GIS unit. Interested in water quality and spatial analysis"
+
+## Platform output
+##3.7.3
+##Windows-10-10.0.17134-SP0
+print(platform.python_version())
+print(platform.platform())
+print(Zack_Conner)
+print(Interests)
+
+## To calculate population variance
+    ## given a set of numbers(p)
+
+    ## 1. set the output to zero
+    ## 2. for each number in set(p)
+        ## a) add number to the output
+    ## 3. return output number
+    ## 4. divide output number by total numbers in p to get mean
+    ## 5. for each number in p,
+        ## a) subtract each value (i) from the mean
+        ## b) square the differences
+    ## 6. return output numbers in set (x)
+    ## 7. for every number in x
+        ## a) add the total
+    ## 8. return output
+    ## 9. divide output by total numbers in set (p)
+    ## 10. print(output)
+
+
